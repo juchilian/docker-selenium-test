ccfe6c46db6cc09bd65302215cc87111ca093172
juchilian <juchilian1227@gmail.com>
2021-08-14 08:57:43 +0900

    first commit

---
+++ app/bot.py
@@ -0,0 +1,12 @@
+from time import sleep
+from selenium.webdriver.common import desired_capabilities
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium import webdriver
+
+sleep(5)
+
+driver = webdriver.Remote('http://selenium:4444/wd/hub',
+                          desired_capabilities=DesiredCapabilities.CHROME)
+
+driver.get("https://python.org")
+driver.save_screenshot('screenshot.png')
